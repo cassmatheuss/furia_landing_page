786b3dce82b8ad7efd4c774b0f90933445deb640
Matheus Castilho <85584099+cassmatheuss@users.noreply.github.com>
2024-08-28 10:14:46 -0300

    fix: zoom in and zoom out

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,18 +4,11 @@ import Sobre from "@/assets/Sobre.svg"
 export function About() {
   return (
     <main id="sobre" className="flex items-center justify-center w-full h-[926px] relative overflow-hidden bg-black">
-      <div className="flex justify-end absolute inset-0 top-20">
-        <Image
-          src={Fallen}
-          alt="Furia Background"
-          className="z-0 bg-cover w-auto h-auto"
-        />
-      </div>
-      <div className="grid place-items-start z-10 w-[1000px] pt-4">
+      <div className="grid pl-40 z-10 w-[1000px] pt-4">
       <Image
           src={Sobre}
           alt="Furia Background"
-          className="pb-6 drop-shadow"
+          className="pb-6 drop-shadow grid"
         />
         <h1 className="text-white text-2xl font-light tracking-wide max-w-[600px]">
         Nossa história é de pioneirismo, grandes conquistas e tradição. Nosso presente é de 
@@ -23,6 +16,15 @@ export function About() {
         business, tecnologia e social são os principais constituintes do movimento FURIA, que representa uma unidade que respeita as individualidades e impacta 
         positivamente os contextos em que se insere. Unimos pessoas e alimentamos sonhos dentro e fora dos jogos.
         </h1>
+      </div>
+      <div>
+        
+          <Image
+            src={Fallen}
+            alt="Furia Background"
+            className="z-0 bg-cover"
+          />
+
       </div>
     </main>
   );
