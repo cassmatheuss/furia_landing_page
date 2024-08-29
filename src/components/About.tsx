ccee3638550562a3f8c9f76dba92d3cdf2f982d5
Matheus Castilho <85584099+cassmatheuss@users.noreply.github.com>
2024-08-29 12:28:49 -0300

    fix: removed mobile fallen image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,13 +19,6 @@ export function About() {
         business, tecnologia e social são os principais constituintes do movimento FURIA, que representa uma unidade que respeita as individualidades e impacta 
         positivamente os contextos em que se insere. Unimos pessoas e alimentamos sonhos dentro e fora dos jogos.
         </h1>
-        <div className="flex justify-center absolute h-full items-center">
-        <Image
-            src={Fallen}
-            alt="Furia Background"
-            className="-z-10 sm:hidden object-cover bg-cover"
-          />
-      </div>
       </div>
        
       <div>
